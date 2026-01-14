6e0a0dfffe3c72b4197267e6f2fcefc7394d4a28
Nick <jaman228822@mail.ru>
2026-01-15 01:59:48 +0500

    fix unused import

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,7 +3,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
-import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginModal } from 'features/AuthByUsername';
 
 interface NavbarProps {
